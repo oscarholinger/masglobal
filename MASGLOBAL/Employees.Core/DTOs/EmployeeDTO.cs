643b48bbd551e62383f7c2890c0a81251307f45c
Oscar Holinger <oscarholinger@gmail.com>
2021-01-28 17:28:37 -0300

    Start

---
+++ MASGLOBAL/Employees.Core/DTOs/EmployeeDTO.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Employees.Core.Entities
+{
+    public class EmployeeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContractTypeName { get; set; }
+        public int RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDescription { get; set; }
+        public decimal HourlySalary { get; set; }
+        public decimal MonthlySalary { get; set; }
+    }
+}
+
+
